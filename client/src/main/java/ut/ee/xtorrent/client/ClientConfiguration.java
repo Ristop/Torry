365df2fedbfa0279dd565ebf6dec1fc0e0c23254
Andreas Baum <andreas.baum95@hotmail.com>
2018-09-27 19:27:49 +0300

    Basic torrent file reader (not completely finished)

--- client/src/main/java/ut/ee/xtorrent/client/ClientConfiguration.java
+++ client/src/main/java/ut/ee/xtorrent/client/ClientConfiguration.java
@@ -4,6 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 @Configuration
 public class ClientConfiguration {
 
@@ -12,6 +16,38 @@ public class ClientConfiguration {
     // Client application logic starts from here
     public ClientConfiguration() {
         log.info("Client Initialized.");
+        torretFilesTest();
+    }
+
+    private void torretFilesTest() {
+        log.info("Torrent files testing started");
+        for (TorrentFIle torrent: getFolderTorrentFiles("client/src/main/resources/torrent_files")) {
+            log.info(torrent.toString());
+        }
+    }
+
+    private ArrayList<TorrentFIle> getFolderTorrentFiles(String folderPath){               //gives only torrent files (not others)
+        ArrayList<TorrentFIle> torrentFiles = new ArrayList<>();
+        File folder = new File(folderPath);
+        File[] listOfFiles = folder.listFiles();
+
+        if (listOfFiles != null) {
+            try {
+                for (File file : listOfFiles) {
+                    if (isTorrentFile(file.getName())) {
+                        String fileLocation = folderPath + "/" + file.getName();
+                        torrentFiles.add(new TorrentFIle(fileLocation));
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return torrentFiles;
+    }
+
+    private boolean isTorrentFile(String filename) {
+        return filename.split("\\.")[1].equals("torrent");
     }
 
 }
