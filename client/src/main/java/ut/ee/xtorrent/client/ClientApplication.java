cac3dec6b1d430616bed193c05fcc726abb33c0e
Egert <egert.teesaar@gmail.com>
2018-10-09 11:48:20 +0300

    Changing files between 2 clients

--- client/src/main/java/ut/ee/xtorrent/client/ClientApplication.java
+++ client/src/main/java/ut/ee/xtorrent/client/ClientApplication.java
@@ -19,14 +19,20 @@ public class ClientApplication {
     protected ClientApplication() {
     }
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
 //        SpringApplication app = new SpringApplication(ClientApplication.class);
 //        app.setBannerMode(Banner.Mode.LOG);
 //        app.run(args);
 
-        new ClientThreadRead(6869).start();
+        int peerServerPort = Integer.parseInt(args[0]);
+        int otherClientPort = Integer.parseInt(args[1]);
+        String hasFile, needsFile;
+        hasFile = args[2];
+      //  String pathToNeededFile = args[3];
+
+        new ClientThreadRead(otherClientPort).start();
         //startClient("localhost",6869);
-        startServerSocket(6868);
+        startServerSocket(peerServerPort, hasFile);
     }
 
     @PreDestroy
@@ -35,14 +41,14 @@ public class ClientApplication {
     }
 
 
-    public static void startServerSocket(int port) throws IOException {
+    public static void startServerSocket(int port, String hasFile) throws IOException {
 
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Server is listening on port " + port);
             while (true) {
                 Socket socket = serverSocket.accept();
                 System.out.println("New client connected");
-                new ClientThreadWrite(socket).start();
+                new ClientThreadWrite(socket, hasFile).start();
             }
         } catch (IOException e) {
             e.printStackTrace();
--- client/src/main/java/ut/ee/xtorrent/client/ClientThreadRead.java
+++ client/src/main/java/ut/ee/xtorrent/client/ClientThreadRead.java
@@ -1,8 +1,8 @@
 package ut.ee.xtorrent.client;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import org.apache.commons.io.FileUtils;
+
+import java.io.*;
 import java.net.Socket;
 
 public class ClientThreadRead extends  Thread{
@@ -15,23 +15,38 @@ public class ClientThreadRead extends  Thread{
     }
 
     public void run() {
-        while(true) {
+
+        boolean notConnected = true;
+        while(notConnected)
             try (
                     Socket socket = new Socket("localhost", port);
-                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+        //            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+
+                    DataInputStream dis = new DataInputStream(socket.getInputStream())
             ) {
+                byte incomingFileLength = dis.readByte();
+                byte[] file = new byte[incomingFileLength];
+                dis.readFully(file);
+
+                FileUtils.writeByteArrayToFile(new File("file_from_other_client.txt"), file);
+           //     System.out.println(new String(file));
+
+                notConnected = false;
+
+             /*
                 String fromServer;
                 while ((fromServer = in.readLine()) != null) {
                     System.out.println("Other client: " + fromServer);
                 }
+
+                */
             } catch (IOException e) {
-                System.out.println("ServerSide not available");
+                System.out.println("Other client not yet available");
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
             }
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
     }
 }
