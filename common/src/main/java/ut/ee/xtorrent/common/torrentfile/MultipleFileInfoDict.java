89959a0529bda8c2f3645fe0e38d3758040f423f
Andreas Baum <andreas.baum95@hotmail.com>
2018-10-10 12:53:12 +0300

    Name was missing also for MultipleFileInfoDic

--- common/src/main/java/ut/ee/xtorrent/common/torrentfile/MultipleFileInfoDict.java
+++ common/src/main/java/ut/ee/xtorrent/common/torrentfile/MultipleFileInfoDict.java
@@ -7,6 +7,7 @@ import java.util.Map;
 public class MultipleFileInfoDict extends InfoDict {
 
     private final List<FileEntry> files;
+    private final String name;               //name of the main directory
 
     @SuppressWarnings("unchecked")
     public MultipleFileInfoDict(Map<String, Object> info) {
@@ -19,6 +20,7 @@ public class MultipleFileInfoDict extends InfoDict {
             fileEntries.add(new FileEntry(fileEntry));
         }
         this.files = fileEntries;
+        this.name = (String) info.get("name");
     }
 
     public List<FileEntry> getFiles() {
@@ -28,7 +30,8 @@ public class MultipleFileInfoDict extends InfoDict {
     @Override
     public String toString() {
         return "MultipleFileInfoDict{" +
-                "files=" + files +
+                "name=" + name +
+                ", files=" + files +
                 '}';
     }
 
