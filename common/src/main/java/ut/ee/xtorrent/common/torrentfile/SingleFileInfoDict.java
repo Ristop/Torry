923932f3e9af003b6cbf46c99d77caadb6da0772
Andreas Baum <andreas.baum95@hotmail.com>
2018-10-10 12:45:29 +0300

    Single torrentfile name field was missing (now fixed)

--- common/src/main/java/ut/ee/xtorrent/common/torrentfile/SingleFileInfoDict.java
+++ common/src/main/java/ut/ee/xtorrent/common/torrentfile/SingleFileInfoDict.java
@@ -7,15 +7,21 @@ public class SingleFileInfoDict extends InfoDict {
 
     private final long length;
     private final String md5sum;
+    private final String name;
 
     public SingleFileInfoDict(Map<String, Object> info) {
         super(info);
         this.length = (long) info.get("length");
+        this.name = (String) info.get("name");
         this.md5sum = info.containsKey("md5sum") ? (String) info.get("md5sum") : null;
     }
 
     public long getLength() {
         return length;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public Optional<String> getMd5sum() {
@@ -29,7 +35,8 @@ public class SingleFileInfoDict extends InfoDict {
     @Override
     public String toString() {
         return "SingleFileInfoDict{" +
-                "length=" + length +
+                "name=" + name +
+                ", length=" + length +
                 ", md5sum='" + md5sum + '\'' +
                 '}';
     }
