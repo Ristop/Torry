7e401176c77f5de2557fb7670c67f3034b808adc
Andreas Baum <andreas.baum95@hotmail.com>
2018-11-20 14:49:08 +0200

    singlefile sending tests + pieces existance tests

--- client/src/test/java/ut/ee/torry/client/PieceHandlerTest.java
+++ client/src/test/java/ut/ee/torry/client/PieceHandlerTest.java
@@ -1,13 +1,25 @@
 package ut.ee.torry.client;
 
-import be.christophedetroyer.torrent.Torrent;
 import be.christophedetroyer.torrent.TorrentParser;
+import org.apache.commons.io.Charsets;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PieceHandlerTest {
@@ -42,12 +54,96 @@ public class PieceHandlerTest {
         assertEquals(nNumbersToSet(4), ph.getExistingPieceIndexes());
     }
 
+    @Test
+    public void testDownloaderHasNoPieces() throws Exception {
+        PiecesHandler ph = new PiecesHandler(TorrentParser.parseTorrent(singleFileTorLoc), downloaderDownloadFolder);
+        assertEquals(0, ph.getExistingPieceIndexes().size());
+        assertEquals(nNumbersToSet(0), ph.getExistingPieceIndexes());
+
+        ph = new PiecesHandler(TorrentParser.parseTorrent(multiFile1TorLoc), downloaderDownloadFolder);
+        assertEquals(0, ph.getExistingPieceIndexes().size());
+        assertEquals(nNumbersToSet(0), ph.getExistingPieceIndexes());
+
+        ph = new PiecesHandler(TorrentParser.parseTorrent(multiFile2TorLoc), downloaderDownloadFolder);
+        assertEquals(0, ph.getExistingPieceIndexes().size());
+        assertEquals(nNumbersToSet(0), ph.getExistingPieceIndexes());
+    }
+
+    @Test
+    public void testSendSingleFilePieceByPiece() throws Exception {
+        PiecesHandler downloader = new PiecesHandler(TorrentParser.parseTorrent(singleFileTorLoc), downloaderDownloadFolder);
+        PiecesHandler sender = new PiecesHandler(TorrentParser.parseTorrent(singleFileTorLoc), seederDownloadFolder);
+        Set<Integer> downloaded = nNumbersToSet(0);
+        Set<Integer> needsDownloading = nNumbersToSet(36);
+        assertEquals(downloaded, downloader.getExistingPieceIndexes());
+        assertEquals(needsDownloading, downloader.getNotExistingPieceIndexes());
+
+        while (needsDownloading.size() != 0) {
+            int randomPieceID = getRandomPieceID(needsDownloading);
+            byte[] pieceBytes = sender.getPieceBytes(randomPieceID);
+            downloader.writePiece(randomPieceID, pieceBytes);
+            downloaded.add(randomPieceID);
+            needsDownloading.remove(randomPieceID);
+            assertEquals(downloaded, downloader.getExistingPieceIndexes());
+            assertEquals(needsDownloading, downloader.getNotExistingPieceIndexes());
+        }
+    }
+
+    @Test
+    public void testCorrectnessOfSingleFileDownlad() throws Exception{
+        /*
+        File seeder = new File(seederDownloadFolder);
+        File[] listOfSeederFiles = seeder.listFiles();
+        File downloader = new File(seederDownloadFolder);
+        File[] listOfDownloaderFiles = downloader.listFiles();
+
+        for (File file : listOfFiles) {
+            if (file.isFile()) {
+                System.out.println(file.getName());
+            }
+        }*/
+        testSendSingleFilePieceByPiece();
+
+        String seededPath = seederDownloadFolder + File.separator + "singleFile.txt";
+        String downloadedPath = downloaderDownloadFolder + File.separator + "singleFile.txt";
+
+        File seeded = new File(seededPath);
+        File downloaded = new File(downloadedPath);
+        byte[] seederBytes = Files.readAllBytes(seeded.toPath());
+        byte[] downloaderBytes = Files.readAllBytes(downloaded.toPath());
+        assertArrayEquals(seederBytes, downloaderBytes);
+
+        List<String> seededLines = Files.readAllLines(Paths.get(seededPath));
+        List<String> downloadedLines = Files.readAllLines(Paths.get(downloadedPath));
+        assertEquals(seededLines, downloadedLines);
+    }
+
     private Set<Integer> nNumbersToSet(int n) {
         Set<Integer> set = new HashSet<>();
         for (int i = 0; i < n; i++) {
             set.add(i);
         }
         return set;
+    }
+
+    private int getRandomPieceID(Set<Integer> list) {
+        if (list.size() >= 1) {
+            int size = list.size();
+            int item = new Random().nextInt(size);
+            int i = 0;
+            for(Integer obj : list)
+            {
+                if (i == item)
+                    return obj;
+                i++;
+            }
+        }
+        return -1;
+    }
+
+    @After
+    public void DeleteMadeFiles() throws IOException {
+        FileUtils.cleanDirectory(new File(downloaderDownloadFolder));
     }
 }
 
