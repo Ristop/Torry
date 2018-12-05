e25d33062b71b9dff6fe30140da3f0bddc287f88
Andreas Baum <andreas.baum95@hotmail.com>
2018-12-06 00:15:24 +0200

    bitfield formed without reading in the whole while (single file + directory)

--- client/src/main/java/ut/ee/torry/client/PiecesHandler.java
+++ client/src/main/java/ut/ee/torry/client/PiecesHandler.java
@@ -105,30 +105,103 @@ public class PiecesHandler {
         }
     }
 
+
     private boolean[] findBitField() {
+        String fullPath = this.downloadFileDir + File.separator + this.torrent.getName();
+        File downloadedTorrent = new File(fullPath);
+        if (downloadedTorrent.isDirectory()) {
+            return findBitFieldFromDirectory(fullPath);
+        } else if (downloadedTorrent.isFile()) {
+            return findBitFieldFromSingleFile(fullPath);
+        } else { // such file/folder does not exist
+            boolean[] bitField = new boolean[piecesCount];
+            Arrays.fill(bitField, false);
+            return bitField;
+        }
+    }
+
+    private boolean[] findBitFieldFromDirectory(String folderLoc) {
         try {
             boolean[] bitField = new boolean[piecesCount];
-            String filePath = downloadFileDir + File.separator + this.torrent.getName();
-            RandomAccessFile file = new RandomAccessFile(filePath, "r");
+            int currentPiece = 0;
+            int bytesReadForPiece = 0;
+            byte[] pieceBytes = new byte[0];
 
+            for (TorrentFile torrentFile : torrent.getFileList()) {
+                String filepath = folderLoc + File.separator + String.join(File.separator, torrentFile.getFileDirs());
+                int bytesReadFromFile = 0;
+                RandomAccessFile file = new RandomAccessFile(filepath, "r");
+
+                // reading bytes from while according to piece size until all bytes from the file are read
+                while (bytesReadFromFile != file.length()) {
+                    int nrOfBytesToRead = calcBytesCount(file.getFilePointer(), bytesReadForPiece, pieceSize, file.length());
+                    byte[] bytes = new byte[nrOfBytesToRead];
+                    file.read(bytes);
+                    bytesReadForPiece += nrOfBytesToRead;
+                    bytesReadFromFile += nrOfBytesToRead;
+                    pieceBytes = ArrayUtils.addAll(pieceBytes, bytes);
+
+                    if (bytesReadForPiece == pieceSize) {  // we have a full piece
+                        Piece piece = new Piece(currentPiece, this.torrent, pieceBytes, this.downloadFileDir);
+                        pieceBytes = new byte[0];
+                        bitField[currentPiece] = piece.isValid();
+                        currentPiece++;
+                        bytesReadForPiece = 0;
+                    }
+                }
+                file.close();
+
+                if (!pieceBytes.equals(new byte[0])) {  // if last piece isn't exactly full, then we need to add it
+                    Piece piece = new Piece(currentPiece, this.torrent, pieceBytes, this.downloadFileDir);
+                    bitField[currentPiece] = piece.isValid();
+                }
+            }
+            return bitField;
+        } catch (IOException e) {
+            boolean[] bitField = new boolean[piecesCount];
+            Arrays.fill(bitField, false);
+            return bitField;
+        }
+    }
+
+    private boolean[] findBitFieldFromSingleFile(String filepath) {
+        try {
+            boolean[] bitField = new boolean[piecesCount];
+            RandomAccessFile file = new RandomAccessFile(filepath, "r");
             byte[] bytes;
+
             for (int i = 0; i < piecesCount; i++) {
-                //int fromBytes = this.pieceSize * i;
-                bytes = new byte[piecesCount];
+                int bytesCount = calcBytesCount(file.getFilePointer(), pieceSize, file.length());
+                bytes = new byte[bytesCount];
                 file.read(bytes);
                 Piece piece = new Piece(i, this.torrent, bytes, this.downloadFileDir);
 
                 // verifying if the bytes really correspond to torrent file metadata
                 bitField[i] = piece.isValid();
             }
-            System.out.println(bitField.length);
             file.close();
-
             return bitField;
         } catch (IOException e) {  // it means that the file doesn't exist, so all bits are 0.
             boolean[] bitField = new boolean[piecesCount];
             Arrays.fill(bitField, false);
             return bitField;
+        }
+    }
+
+    private int calcBytesCount(long currentPosition, int pieceSize, long fileLength) {
+        if (currentPosition + pieceSize > fileLength) {
+            return (int) (fileLength - currentPosition);
+        } else {
+            return pieceSize;
+        }
+    }
+
+    private int calcBytesCount(long currentPosition, int bytesAlreadyRead, int pieceSize, long fileLength) {
+        long bytesToRead = fileLength - currentPosition;
+        if (bytesToRead + bytesAlreadyRead > pieceSize){  //can fill the whole piece (can't use all new bytes)
+            return pieceSize - bytesAlreadyRead;
+        } else { // can use all new bytes
+            return (int) bytesToRead;
         }
     }
 
